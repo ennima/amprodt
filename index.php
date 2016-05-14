eba83eb2f56b4e1c1f69e4975d6b789069716c1d
Ennima <ennima@hotmail.com>
2016-05-13 20:51:55 -0500

    css

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo('language'); ?>">
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+	<title><?php bloginfo('name'); ?></title>
+	<link rel="shortcut icon" href="<?php bloginfo('siteurl'); ?>/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo(stylesheet_url);?>">
+</head>
+<body>
+	<h1><?php bloginfo('name'); ?></h1>
+	<h2><?php bloginfo('description'); ?></h2>
+	<ul>
+		<li><?php bloginfo('name'); ?></li>
+		<li><?php bloginfo('description'); ?></li>
+		<li><?php bloginfo('wpurl'); ?></li>
+		<li><?php bloginfo('url'); ?></li>
+		<li><?php bloginfo('admin_email'); ?></li>
+		<li><?php bloginfo('charset'); ?></li>
+		<li><?php bloginfo('version'); ?></li>
+		<li><?php bloginfo('html_type'); ?></li>
+		<li><?php bloginfo('language'); ?></li>
+		<li><?php bloginfo('stylesheet_directory'); ?></li>
+		<li><?php bloginfo('stylesheet_url'); ?></li>
+		<li><?php bloginfo('template_directory'); ?></li>
+		<li><?php bloginfo('template_url'); ?></li>
+		<li><?php bloginfo('pingback_url'); ?></li>
+		<li><?php bloginfo('atom_url'); ?></li>
+		<li><?php bloginfo('rdf_url'); ?></li>
+		<li><?php bloginfo('rss_url'); ?></li>
+		<li><?php bloginfo('rss2_url'); ?></li>
+		<li><?php bloginfo('comments_atom_url'); ?></li>
+		<li><?php bloginfo('comments_rss2_url'); ?></li>
+		<li><?php bloginfo('siteurl'); ?></li>
+		<li><?php bloginfo('home'); ?></li>
+
+	</ul>
+</body>
+</html>
